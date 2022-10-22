e225b78b112ee8ef41f9b1d464053f92be2ccf8c
Nathan Hynes <nathanmail@hotmail.co.uk>
2022-10-22 18:19:17 +0100

    add sass

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Form } from './components/forms'
-import './App.css';
+import './App.scss';
 
 const App = () => {
   const [images, setImages] = useState();
