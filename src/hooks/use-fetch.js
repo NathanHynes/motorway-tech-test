b53a3c66e82747b210e50419a9a167b8b1cc88c3
Nathan Hynes <nathanmail@hotmail.co.uk>
2022-10-25 09:31:35 +0100

    add performance logs to useFetch hook

--- src/hooks/use-fetch.js
+++ src/hooks/use-fetch.js
@@ -2,32 +2,35 @@ import {useState, useEffect, useRef} from 'react'
 
 export const useFetch = (url) => {
   const cache = useRef({})
-  const [status, setStatus] = useState('idle');
-  const [data, setData] = useState([]);
+  const [status, setStatus] = useState('idle')
+  const [data, setData] = useState([])
 
   useEffect(() => {
-    if (!url) return;
+    if (!url) return
       const fetchData = async () => {
+        console.time('fetch-images')
             try {
               if (cache.current[url]) {
                 console.log('CACHE HIT')
-                const data = cache.current[url];
+                const data = cache.current[url]
+                console.timeEnd('fetch-images')
                 setData(data);
                 setStatus('fetched');
               } else {
                 console.log('CACHE MISS')
-                const response = await fetch(url);
-                const data = await response.json();
-                cache.current[url] = data; // set response in cache;
-                setData(data);
-                setStatus('fetched');
+                const response = await fetch(url)
+                const data = await response.json()
+                console.timeEnd('fetch-images')
+                cache.current[url] = data // set response in cache;
+                setData(data)
+                setStatus('fetched')
               }
             } catch (error) {
               console.error('Error:', error)
             }
         }
-        fetchData();
-    }, [url]);
+        fetchData()
+    }, [url])
 
-    return { status, data };
+    return { status, data }
 };
